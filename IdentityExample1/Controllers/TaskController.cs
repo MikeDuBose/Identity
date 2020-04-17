2264bd703986a38d39511859483fb636cf81691a
Michael DuBose <michaeldubose@quickenloans.com>
2020-04-17 12:00:34 -0400

    Working search function

---
+++ IdentityExample1/Controllers/TaskController.cs
@@ -0,0 +1,120 @@
+﻿using IdentityExample1.Models.AccountViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using Identity.Dapper.Entities;
+using IdentityExample1.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace IdentityExample1.Controllers
+{
+    public class TaskController : Controller
+    {
+        int result;
+
+        private DAL dal;
+
+        private readonly UserManager<DapperIdentityUser> _userManager;
+        private readonly SignInManager<DapperIdentityUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public TaskController(
+            UserManager<DapperIdentityUser> userManager,
+            SignInManager<DapperIdentityUser> signInManager,
+            ILoggerFactory loggerFactory,
+            IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = loggerFactory.CreateLogger<AccountController>();
+            dal = new DAL(config.GetConnectionString("DefaultConnection"));
+        }
+        public IActionResult Index()
+        {
+            ViewData["Name"] = User.Identity.Name;
+            
+            ViewData["UID"] = _userManager.GetUserId(User);
+
+            
+
+            if (ViewData["Name"] != null)
+            {
+                int uId = int.Parse(_userManager.GetUserId(User));
+                IEnumerable<UserTask> utasks = dal.GetTasks(uId);
+                ViewData["UTasks"] = utasks;
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        //Add Task Get
+        public IActionResult AddForm()
+        {
+            ViewData["Name"] = User.Identity.Name;
+
+            ViewData["UID"] = _userManager.GetUserId(User);
+            return View(new UserTask());
+        }
+        //Add Task Post
+        [HttpPost]
+        public IActionResult AddTask(UserTask u)
+        {
+            ViewData["Name"] = User.Identity.Name;
+
+            ViewData["UID"] = _userManager.GetUserId(User);
+            u.UserId = int.Parse(_userManager.GetUserId(User));
+            u.TaskStatus = 0;
+
+            result = dal.AddTask(u);
+            
+            return RedirectToAction("Index", u);
+        }
+
+        public IActionResult CompleteTask(UserTask u)
+        {
+            u.UserId = int.Parse(_userManager.GetUserId(User));
+
+            dal.CompleteTask(u.Id);
+            ViewData["Name"] = User.Identity.Name;
+            ViewData["UID"] = _userManager.GetUserId(User);
+            IEnumerable<UserTask> utasks = dal.GetAllTasksById((string)ViewData["UID"]);
+            ViewData["Tasks"] = utasks;
+            return RedirectToAction("Index", u);
+        }
+
+        //Delete Task
+        public IActionResult Delete(int id)
+        {
+            int result = dal.DeleteTaskById(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditForm()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string search)
+        {
+            ViewData["Name"] = User.Identity.Name;
+            ViewData["UID"] = _userManager.GetUserId(User);
+            IEnumerable<UserTask> results = dal.GetSearchResults(search, (string)ViewData["UID"]);
+            ViewData["Results"] = results;
+
+            return View();
+        }
+
+    }
+}
---
+++ IdentityExample1/Models/DAL.cs
@@ -0,0 +1,64 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample1.Models
+{
+    public class DAL
+    {
+        private SqlConnection conn;
+        
+
+        public DAL(string connectionString)
+        {
+            conn = new SqlConnection(connectionString);
+        }
+
+        public int AddTask(UserTask u)
+        {
+            string addQuery = "INSERT INTO IdentityTasks (UserId, TaskDescription, DueDate, TaskStatus) ";
+            addQuery += "VALUES(@UserId, @TaskDescription, @DueDate, @TaskStatus)";
+
+            return conn.Execute(addQuery, u);
+        }
+
+        public IEnumerable<UserTask> GetTasks(int id)
+        {
+            string queryString = "SELECT * FROM IdentityTasks WHERE UserId = @val";
+
+            return conn.Query<UserTask>(queryString, new { val = id });
+        }
+
+        public int DeleteTaskById(int id)
+        {
+            string deleteString = "DELETE FROM IdentityTasks WHERE Id = @val";
+            return conn.Execute(deleteString, new { val = id });
+        }
+
+        public void CompleteTask(int id)
+        {
+
+            string queryString = "Update IdentityTasks SET TaskStatus = '1' WHERE id = @id";
+            conn.QueryFirstOrDefault<UserTask>(queryString, new { id = id });
+
+
+        }
+
+        public IEnumerable<UserTask> GetAllTasksById(string userid)
+        {
+
+            string queryString = "SELECT * FROM IdentityTasks WHERE UserId = @userid";
+            return conn.Query<UserTask>(queryString, new { userid = userid });
+        }
+
+        public IEnumerable<UserTask> GetSearchResults(string search, string userid)
+        {
+            string queryString = "Select * FROM IdentityTasks WHERE UserID = @userid AND TaskDescription LIKE '%' +@search + '%'";
+
+            return conn.Query<UserTask>(queryString, new { search = search, userid = userid });
+        }
+    }
+}
